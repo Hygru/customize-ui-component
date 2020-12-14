dd28bf8aa3dbcdd95a7e1815be9671ddce728c16
jiangjiang <jiangjiang@dazhuanjia.com>
2020-12-14 16:25:36 +0800

    fix: 修改代码校验

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,63 @@
-const path = require("path")
-
 module.exports = {
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        project: path.resolve(__dirname, "./tsconfig.json"),
-        tsconfigRootDir: __dirname,
-        ecmaVersion: 2019,
-        sourceType: "module",
-        // ecmaFeatures: {
-        //     jsx: true,
-        //     experimentalObjectRestSpread: true
-        // }
-    },
+    parser: '@typescript-eslint/parser',
     extends: [
-        // 使用推荐的React代码检测规范
-        "plugin:@typescript-eslint/recommended"
+    // 使用推荐的React代码检测规范
+        'airbnb-typescript',
+        'airbnb/hooks',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking'
     ],
-    plugins: ["@typescript-eslint", "react", "react-hooks"],
+    plugins: ['@typescript-eslint', 'react', 'react-hooks', 'jsx-a11y', 'import'],
     env: {
-        // 你的环境变量（包含多个预定义的全局变量）
+    // 你的环境变量（包含多个预定义的全局变量）
         browser: true,
-        node: true
-        // mocha: true,
-        // jest: true,
-        // jquery: true
+        node: true,
+        es6: true,
     },
     globals: {
-        // 你的全局变量（设置为 false 表示它不允许被重新赋值）
+    // 你的全局变量（设置为 false 表示它不允许被重新赋值）
+    },
+    parserOptions: {
+        project: './tsconfig.eslint.json',
+        tsconfigRootDir: __dirname,
+        ecmaVersion: 2019,
+        sourceType: 'module',
+        ecmaFeatures: {
+            experimentalObjectRestSpread: true,
+        },
     },
     rules: {
-        // 自定义你的规则
-    }
+    // 自定义你的规则
+        'arrow-parens': 'off',
+        semi: ['error', 'never'],
+        indent: [
+            'error',
+            4,
+            {
+                ignoredNodes: ['TemplateLiteral'],
+            },
+        ],
+        'max-len': [
+            'error',
+            120,
+            4,
+            {
+                ignoreUrls: true,
+                ignoreComments: false,
+                ignoreTrailingComments: true,
+                ignoreRegExpLiterals: true,
+                ignoreStrings: true,
+                ignoreTemplateLiterals: true,
+            },
+        ],
+        '@typescript-eslint/semi': ['error', 'never'],
+        '@typescript-eslint/indent': [
+            'error',
+            4,
+            {
+                ignoredNodes: ['TemplateLiteral'],
+            },
+        ],
+        'react/require-default-props': ['off'],
+    },
 }
--- src/Test/Test.tsx
+++ src/Test/Test.tsx
@@ -5,8 +5,6 @@ interface IProps {
     text?: string
 }
 
-const Test: React.SFC<IProps> = ({ text = 'test' }: IProps) => {
-  return <div className="test">{text}</div>
-}
+const Test: React.SFC<IProps> = ({ text }: IProps) => <div className="test">{text}</div>
 
 export default Test
