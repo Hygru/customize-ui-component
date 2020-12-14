493b16bc4e09ebba27176c4944957ac254935178
jiangjiang <jiangjiang@dazhuanjia.com>
2020-12-14 16:35:01 +0800

    fix: 修改代码校验

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     extends: ["stylelint-config-standard"],
     rules: {
         // 自定义规则
+        indentation: 4,
     },
     ignoreFiles: ["node_modules/**/*", "lib/**/*", "**/*.js", "**/*.jsx", "**/*.tsx", "**/*.ts"]
 }
